15ea0b23cc7f6ae24e8f6a4d6b772e0346a0cd12
houfeng <houfeng@flashhold.com>
2022-07-21 00:17:20 +0800

    init

--- producer/src/main/java/com/quicktron/producer/TransactionApplication.java
+++ producer/src/main/java/com/quicktron/producer/TransactionApplication.java
@@ -1,11 +1,13 @@
 package com.quicktron.producer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan(basePackages = "com.quicktron.producer.mapper")
 public class TransactionApplication {
     
     public static void main(String[] args) {
